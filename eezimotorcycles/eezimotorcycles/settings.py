0d51aa063f4f33ec6efb1bcccb5ffb8d9c28e7a7
Alan Staunton <l00177667@atu.ie>
2022-11-19 14:43:29 +0000

    Added empty line jsut to push to github 2

--- eezimotorcycles/eezimotorcycles/settings.py
+++ eezimotorcycles/eezimotorcycles/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# THIS IS A SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '&#i-3g4)sc*325zo+=)5=ha837!23s=+ok*e+%9ro!)v51(1(j'
 
 
