85d9629bc6e7c5b4633f50ac2ee61ea2736a491e
Alan Staunton <l00177667@atu.ie>
2022-11-19 14:42:01 +0000

    Added empty line jsut to push to github

--- eezimotorcycles/eezimotorcycles/settings.py
+++ eezimotorcycles/eezimotorcycles/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '&#i-3g4)sc*325zo+=)5=ha837!23s=+ok*e+%9ro!)v51(1(j'
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
